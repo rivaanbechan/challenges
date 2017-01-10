c50346cc64592685e8da414c9ed8069c73ae3b60
Rivaan Bechan <rivaan.bechan@gsoc.co.za>
2017-01-10 10:18:12 +0200

    forgot to paste into file

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,18 +1,26 @@
-from data import DICTIONARY, LETTER_SCORES
+from data import DICTIONARY, LETTER_SCORES                                                                                                                                                     
 
 def load_words():
     """Load dictionary into a list and return list"""
-    pass
+    with open(DICTIONARY) as f:
+        dict_words = [line.strip() for line in f]
+    return dict_words
 
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    calc = 0 
+    word = word.upper()
+    for char in word:
+        for key in LETTER_SCORES.items():
+            if char == key[0]:
+                calc += int(key[1])
+    return calc
 
-def max_word_value():
+def max_word_value(max_word=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    return max(max_word, key=calc_word_value)
 
 if __name__ == "__main__":
     pass # run unittests to validate
